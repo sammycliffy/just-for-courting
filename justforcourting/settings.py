277af05e9c0b5ec1d54033617aa4189f62ab2b1d
Sammy_cliffy <sammycliffy15@gmail.com>
2018-06-12 20:34:19 +0000

    second

--- justforcourting/settings.py
+++ justforcourting/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'justforcourting.urls'
